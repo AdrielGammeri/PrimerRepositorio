e87de6a5b0df11042c5fbf8eee759260151e9b53
Adriel Gammeri <adrielgammeri@outlook.com>
2023-01-22 20:12:18 -0300

    Backend parte 2

---
+++ M6M2/Art Symbol 2.0/frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from "./Componentes/layout/Header";
+import Nav from "./Componentes/layout/Nav";
+import Footer from "./Componentes/layout/Footer";
+
+import Contactopage from "./Pages/Contactopage";
+import Galeriapage from "./Pages/Galeriapage";  
+import Homepage from "./Pages/Homepage";
+import Nosotropage from "./Pages/Nosotrospage";
+import Serviciospages from "./Pages/Serviciospages";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/contacto" element={<Contactopage/>}/>
+        <Route path="/galeria" element={<Galeriapage/>}/>
+        <Route path="/nosotros" element={<Nosotropage/>}/>
+        <Route path="/servicios" element={<Serviciospages/>}/>
+      </Routes>
+      </BrowserRouter>
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ M6M2/Art Symbol 2.0/frontend/src/Pages/Serviciospages.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "../Styles/servicios.css";
+
+const Serviciospages = (props) =>{
+    return(
+        <div class="holder">
+        <div class="Cont_body">
+          <div class="Cont_B1">
+
+
+             <div class="card mb-3">
+               <div class="row g-0">
+                 <div class="Cont_Img1">
+                   <img class="F1" src="img.css/Art_ARTISTAS/A2.png"  alt="..."/>
+                 </div>
+                 <div class="col-md-8">
+                   <div class="card-body">
+                      <h3 class="card-title">David Hockney</h3>
+                      <h5 class="card-title2" ></h5>
+                      <p class="card-text">Es el cuarto de cinco hijos de Laura y Kenneth Hockney (él, un objetor de conciencia en la segunda guerra mundial).11​12​ Fue educado en la Escuela Primaria de Wellington, Bradford Grammar School, Universidad de Bradford de Arte y en el Royal College of Art en Londres, donde conoció a R. B. Kitaj.11​ Mientras estuvo ahí, Hockney dijo haberse sentido en casa y orgulloso de su trabajo. En el Royal College of Art, Hockney fue incluido en la exposición Jóvenes contemporáneos, junto con Peter Blake, anunciando la llegada del Arte Pop británico.A él se le asociaba con este movimiento, pero sus recientes obras tenían elementos del expresionismo abstracto, parecido a algunos trabajos de Francis Bacon. Cuando la RCA le advirtió de que no podían permitir que se graduase en 1962, Hockney creó el dibujo conocido como "El Diploma" en forma de protesta. Había rehusado escribir un ensayo requerido para el examen final, argumentando que debía ser evaluado sobre la base de sus obras únicamente. En reconocimiento a su talento, la RCA cambió sus reglas y le entregó su diploma..</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="card mb-3">
+               <div class="row g-0">
+                 <div class="Cont_Img1">
+                   <img class="F1" src="img.css/Art_ARTISTAS/A1.png"  alt="..."/>
+                 </div>
+                 <div class="col-md-8">
+                   <div class="card-body">
+                      <h3 class="card-title">David Hockney</h3>
+                      <h5 class="card-title2" ></h5>
+                      <p class="card-text">Es el cuarto de cinco hijos de Laura y Kenneth Hockney (él, un objetor de conciencia en la segunda guerra mundial).11​12​ Fue educado en la Escuela Primaria de Wellington, Bradford Grammar School, Universidad de Bradford de Arte y en el Royal College of Art en Londres, donde conoció a R. B. Kitaj.11​ Mientras estuvo ahí, Hockney dijo haberse sentido en casa y orgulloso de su trabajo. En el Royal College of Art, Hockney fue incluido en la exposición Jóvenes contemporáneos, junto con Peter Blake, anunciando la llegada del Arte Pop británico.A él se le asociaba con este movimiento, pero sus recientes obras tenían elementos del expresionismo abstracto, parecido a algunos trabajos de Francis Bacon. Cuando la RCA le advirtió de que no podían permitir que se graduase en 1962, Hockney creó el dibujo conocido como "El Diploma" en forma de protesta. Había rehusado escribir un ensayo requerido para el examen final, argumentando que debía ser evaluado sobre la base de sus obras únicamente. En reconocimiento a su talento, la RCA cambió sus reglas y le entregó su diploma..</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="card mb-3">
+               <div class="row g-0">
+                 <div class="Cont_Img1">
+                   <img class="F1" src="img.css/Art_ARTISTAS/A3.png"  alt="..."/>
+                 </div>
+                 <div class="col-md-8">
+                   <div class="card-body">
+                      <h3 class="card-title">David Hockney</h3>
+                      <h5 class="card-title2" ></h5>
+                      <p class="card-text">Es el cuarto de cinco hijos de Laura y Kenneth Hockney (él, un objetor de conciencia en la segunda guerra mundial).11​12​ Fue educado en la Escuela Primaria de Wellington, Bradford Grammar School, Universidad de Bradford de Arte y en el Royal College of Art en Londres, donde conoció a R. B. Kitaj.11​ Mientras estuvo ahí, Hockney dijo haberse sentido en casa y orgulloso de su trabajo. En el Royal College of Art, Hockney fue incluido en la exposición Jóvenes contemporáneos, junto con Peter Blake, anunciando la llegada del Arte Pop británico.A él se le asociaba con este movimiento, pero sus recientes obras tenían elementos del expresionismo abstracto, parecido a algunos trabajos de Francis Bacon. Cuando la RCA le advirtió de que no podían permitir que se graduase en 1962, Hockney creó el dibujo conocido como "El Diploma" en forma de protesta. Había rehusado escribir un ensayo requerido para el examen final, argumentando que debía ser evaluado sobre la base de sus obras únicamente. En reconocimiento a su talento, la RCA cambió sus reglas y le entregó su diploma..</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+
+              
+              <div class="card mb-3">
+               <div class="row g-0">
+                 <div class="Cont_Img1">
+                   <img class="F1" src="img.css/Art_ARTISTAS/A4.png"  alt="..."/>
+                 </div>
+                 <div class="col-md-8">
+                   <div class="card-body">
+                      <h3 class="card-title">David Hockney</h3>
+                      <h5 class="card-title2" ></h5>
+                      <p class="card-text">Es el cuarto de cinco hijos de Laura y Kenneth Hockney (él, un objetor de conciencia en la segunda guerra mundial).11​12​ Fue educado en la Escuela Primaria de Wellington, Bradford Grammar School, Universidad de Bradford de Arte y en el Royal College of Art en Londres, donde conoció a R. B. Kitaj.11​ Mientras estuvo ahí, Hockney dijo haberse sentido en casa y orgulloso de su trabajo. En el Royal College of Art, Hockney fue incluido en la exposición Jóvenes contemporáneos, junto con Peter Blake, anunciando la llegada del Arte Pop británico.A él se le asociaba con este movimiento, pero sus recientes obras tenían elementos del expresionismo abstracto, parecido a algunos trabajos de Francis Bacon. Cuando la RCA le advirtió de que no podían permitir que se graduase en 1962, Hockney creó el dibujo conocido como "El Diploma" en forma de protesta. Había rehusado escribir un ensayo requerido para el examen final, argumentando que debía ser evaluado sobre la base de sus obras únicamente. En reconocimiento a su talento, la RCA cambió sus reglas y le entregó su diploma..</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="card mb-3">
+               <div class="row g-0">
+                 <div class="Cont_Img1">
+                   <img class="F1" src="img.css/Art_ARTISTAS/A5.png"  alt="..."/>
+                 </div>
+                 <div class="col-md-8">
+                   <div class="card-body">
+                      <h3 class="card-title">David Hockney</h3>
+                      <h5 class="card-title2" ></h5>
+                      <p class="card-text">Es el cuarto de cinco hijos de Laura y Kenneth Hockney (él, un objetor de conciencia en la segunda guerra mundial).11​12​ Fue educado en la Escuela Primaria de Wellington, Bradford Grammar School, Universidad de Bradford de Arte y en el Royal College of Art en Londres, donde conoció a R. B. Kitaj.11​ Mientras estuvo ahí, Hockney dijo haberse sentido en casa y orgulloso de su trabajo. En el Royal College of Art, Hockney fue incluido en la exposición Jóvenes contemporáneos, junto con Peter Blake, anunciando la llegada del Arte Pop británico.A él se le asociaba con este movimiento, pero sus recientes obras tenían elementos del expresionismo abstracto, parecido a algunos trabajos de Francis Bacon. Cuando la RCA le advirtió de que no podían permitir que se graduase en 1962, Hockney creó el dibujo conocido como "El Diploma" en forma de protesta. Había rehusado escribir un ensayo requerido para el examen final, argumentando que debía ser evaluado sobre la base de sus obras únicamente. En reconocimiento a su talento, la RCA cambió sus reglas y le entregó su diploma..</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="card mb-3">
+               <div class="row g-0">
+                 <div class="Cont_Img1">
+                   <img class="F1" src="img.css/Art_ARTISTAS/A6.png"  alt="..."/>
+                 </div>
+                 <div class="col-md-8">
+                   <div class="card-body">
+                      <h3 class="card-title">David Hockney</h3>
+                      <h5 class="card-title2" ></h5>
+                      <p class="card-text">Es el cuarto de cinco hijos de Laura y Kenneth Hockney (él, un objetor de conciencia en la segunda guerra mundial).11​12​ Fue educado en la Escuela Primaria de Wellington, Bradford Grammar School, Universidad de Bradford de Arte y en el Royal College of Art en Londres, donde conoció a R. B. Kitaj.11​ Mientras estuvo ahí, Hockney dijo haberse sentido en casa y orgulloso de su trabajo. En el Royal College of Art, Hockney fue incluido en la exposición Jóvenes contemporáneos, junto con Peter Blake, anunciando la llegada del Arte Pop británico.A él se le asociaba con este movimiento, pero sus recientes obras tenían elementos del expresionismo abstracto, parecido a algunos trabajos de Francis Bacon. Cuando la RCA le advirtió de que no podían permitir que se graduase en 1962, Hockney creó el dibujo conocido como "El Diploma" en forma de protesta. Había rehusado escribir un ensayo requerido para el examen final, argumentando que debía ser evaluado sobre la base de sus obras únicamente. En reconocimiento a su talento, la RCA cambió sus reglas y le entregó su diploma..</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="card mb-3">
+               <div class="row g-0">
+                 <div class="Cont_Img1">
+                   <img class="F1" src="img.css/Art_ARTISTAS/A7.png"  alt="..."/>
+                 </div>
+                 <div class="col-md-8">
+                   <div class="card-body">
+                      <h3 class="card-title">David Hockney</h3>
+                      <h5 class="card-title2" ></h5>
+                      <p class="card-text">Es el cuarto de cinco hijos de Laura y Kenneth Hockney (él, un objetor de conciencia en la segunda guerra mundial).11​12​ Fue educado en la Escuela Primaria de Wellington, Bradford Grammar School, Universidad de Bradford de Arte y en el Royal College of Art en Londres, donde conoció a R. B. Kitaj.11​ Mientras estuvo ahí, Hockney dijo haberse sentido en casa y orgulloso de su trabajo. En el Royal College of Art, Hockney fue incluido en la exposición Jóvenes contemporáneos, junto con Peter Blake, anunciando la llegada del Arte Pop británico.A él se le asociaba con este movimiento, pero sus recientes obras tenían elementos del expresionismo abstracto, parecido a algunos trabajos de Francis Bacon. Cuando la RCA le advirtió de que no podían permitir que se graduase en 1962, Hockney creó el dibujo conocido como "El Diploma" en forma de protesta. Había rehusado escribir un ensayo requerido para el examen final, argumentando que debía ser evaluado sobre la base de sus obras únicamente. En reconocimiento a su talento, la RCA cambió sus reglas y le entregó su diploma..</p>
+                   </div>
+                  </div>
+                </div>
+              </div>
+
+
+          </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+      </div>
+    )
+}
+export default Serviciospages;
